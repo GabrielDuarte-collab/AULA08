2a6a116e087bb371b1a631bcf3cea208c54575c7
GabrielDuarte-collab <joaogabr44@gmail.com>
2025-12-10 00:41:31 +0000

    void

---
+++ Objetivo1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Objetivo1 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Insira um numero N: ");
+    int N = sc.nextInt();
+    int soma = 0;
+    
+    for (int 1 + 1 <= N; 1++) {
+        soma = soma + 1;
+    }
+    System.out.printf("A soma e igual a, %d\n, soma");
+
+    sc.close();
+ }
+}
